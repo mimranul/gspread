b729a8554a7e290ad70c71e4081d206c808618eb
mimranul <mimranul@gmail.com>
2023-06-23 21:12:10 -0400

    moved the google_sheets_utility_test.py file to appropriate package

--- tests/google_sheets_utility_tests/google_sheets_utility_test.py
+++ tests/google_sheets_utility_tests/google_sheets_utility_test.py
@@ -1,13 +1,17 @@
 import os
 
-
 import pytest
 
 from src.google_sheets_utility.delivery_sheet import DeliverySheet
 
-from .conftest import GspreadTest
+from tests.conftest import GspreadTest
 
-class SpreadsheetTest(GspreadTest):
+"""
+This test is responsible for sanity checking.
+"""
+
+
+class SpreadsheetSanityTest(GspreadTest):
 
     @pytest.mark.vcr()
     def test_secret_file_exists(self):
@@ -27,12 +31,24 @@ class SpreadsheetTest(GspreadTest):
     @pytest.mark.vcr()
     def test_file_does_not_have_zero_rows(self):
         delivery_sheet = DeliverySheet()
-        ## Need to add logic about rows
-        self.assertTrue(True)
+        self.assertTrue(delivery_sheet.delivery_sheet.row_count != 0)
 
     @pytest.mark.vcr()
-    def test_file_does_not_have_zero_coloumns(self):
+    def test_file_does_not_have_zero_columns(self):
         delivery_sheet = DeliverySheet()
-        ## Need to add logic about coloumns
-        self.assertTrue(True)
+        self.assertTrue(delivery_sheet.delivery_sheet.col_count != 0)
 
+    @pytest.mark.vcr()
+    def test_file_does_not_have_zero_rows(self):
+        delivery_sheet = DeliverySheet()
+        self.assertTrue(delivery_sheet.delivery_sheet.row_count != 0)
+
+    @pytest.mark.vcr()
+    def test_file_has_more_than_four_columns(self):
+        delivery_sheet = DeliverySheet()
+        self.assertTrue(delivery_sheet.delivery_sheet.col_count > 4)
+
+    @pytest.mark.vcr()
+    def test_file_has_more_than_four_rows(self):
+        delivery_sheet = DeliverySheet()
+        self.assertTrue(delivery_sheet.delivery_sheet.row_count > 4)
